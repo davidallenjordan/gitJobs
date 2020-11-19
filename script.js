052de9895aab3658468b0f52d38cde246940b6c4
David Allen-Jordan <davidallenjordan@gmail.com>
2020-11-19 09:37:36 -0500

    Added displayResults function

--- script.js
+++ script.js
@@ -22,18 +22,69 @@
         }
         response.json().then(function(data) {
           console.log(data);
+          app.displayResults(data);
         });
       }
       )
       .catch(function(err) {
         console.log('Fetch Error', err);
       });
+
+    };
+
+    // Display job results on the page
+    app.displayResults = (jobData) => {
+      const jobPostings = document.querySelector('.jobPostings');
+      
+      jobData.forEach(job => {
+        const { 
+          company,
+          company_logo, 
+          company_url, 
+          created_at, 
+          description, 
+          how_to_apply, 
+          id, 
+          location, 
+          title, 
+          type, 
+          url 
+        } = job;
+
+        // Trim down the created_at string
+        const created = created_at.substr(0,9);
+
+        const jobPost = `
+          <div class="jobPost">
+            <img src="${company_logo}"></img>
+            <p>${created}</p>
+            <p>${type}</p>
+            <p>${title}</p>
+            <p>${company}</p>
+            <p>${location}</p>
+          </div>
+        `;
+
+        jobPostings.insertAdjacentHTML('beforebegin', jobPost);
+
+      });
+    }
+
+    app.handleJopPostClick = () => {
+      const jobPost = document.querySelectorAll('.jobPost');
+      console.log(jobPost);
+      jobPost.addEventListener('click', () => {
+        
+      });
+
     };
     
     // Initialize the app
     app.init = () => {
       app.getJobs();
+      app.handleJopPostClick();
     }
+
     
     app.init();
     
