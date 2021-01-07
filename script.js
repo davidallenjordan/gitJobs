8c3ad73d2ce4c15ca288542508d1bffd493f3720
David Allen-Jordan <davidallenjordan@gmail.com>
2021-01-07 18:06:40 -0500

    added click listener and jobDetails functions in js

--- script.js
+++ script.js
@@ -8,9 +8,9 @@
   const app = () => {};
   
   // Get results from github API
-  app.getJobs = () => {
-    const proxyurl = 'https://cors-anywhere.herokuapp.com/';
-    const url = 'https://jobs.github.com/positions.json/?description=python&location=new+york';
+  app.getJobData = () => {
+    const proxyurl = 'https://ancient-oasis-20473.herokuapp.com/';
+    const url = 'https://jobs.github.com/positions.json/';
     
     fetch(proxyurl + url)
     .then(
@@ -23,7 +23,7 @@
         response.json().then(function(data) {
           console.log(data);
           app.removePlaceholders();
-          app.displayResults(data);
+          app.displayJobs(data);
         });
       }
       )
@@ -38,11 +38,17 @@
     app.removePlaceholders = () => {
       const jobPostings = document.querySelector('.jobPostings');
       jobPostings.innerHTML = '';
+
+      // const jobDescription = document.querySelector('.jobDescription');
+      // jobDescription.innerHTML = '';
     }
 
     // Display job results on the page
-    app.displayResults = (jobData) => {
+    app.displayJobs = (jobData) => {
       const jobPostings = document.querySelector('.jobPostings');
+      // const jobDescription = document.querySelector('.jobDescription');
+      const posts = [];
+      let postID = 0;
       
       jobData.forEach(job => {
         const { 
@@ -63,7 +69,7 @@
         const created = created_at.substr(0,9);
 
         const jobPost = `
-          <div class="jobPost">
+          <div class="jobPost" data-id="${postID}">
             <img src="${company_logo}" alt="Logo for ${company}"></img>
             <p>${created} - </p>
             <p>${type}</p>
@@ -73,26 +79,78 @@
           </div>
         `;
 
+        const jobDetails = `
+          <div class="jobHeader">
+            <img src="${company_logo}" alt="Logo for ${company}"></img>
+            <p>${company}</p>
+            <p>${company_url}</p>
+            <a href="${company_url}">Company Site</a>
+          </div>
+
+          <div class="jobDetails">
+            <p>${created} - </p>
+            <p>${type}</p>
+            <p>${title}</p>
+            <p>${location}</p>
+            ${how_to_apply}
+            <p>${description}</p>
+            <p>Job Posting ID: ${id}</p>
+        `
+
+        const posting = {
+          jobPost: jobPost,
+          jobDetails: jobDetails
+        };
+
+        posts.push(posting);
+        postID++;
+
         // insert rendered postings into jobPostings
-        jobPostings.insertAdjacentHTML("afterbegin", jobPost);
+        jobPostings.insertAdjacentHTML('beforeend', jobPost);
+        // jobDescription.insertAdjacentHTML('afterbegin', jobDetails);
+
       });
+
+      console.log(posts);
+
+      app.handleJopPostClick(posts);
     }
 
-    // On jobPost click render information on details container.
+    app.removeDetails = () => {
+      const jobDescription = document.querySelector('.jobDescription');
+      jobDescription.innerHTML = '';
+    }
 
-    app.handleJopPostClick = () => {
-      const jobPost = document.querySelectorAll('.jobPost');
-      console.log(jobPost);
-      // jobPost.addEventListener('click', () => {
-        
-      // });
+    // On jobPost-click render information in details container
+    app.handleJopPostClick = (posts) => {
 
+
+      const jobPosts = document.querySelectorAll('.jobPost');
+
+      function showDetails(event) {      
+        app.removeDetails();
+
+        const clickedJobPost = event.currentTarget;
+        // clickedJobPost.closest('.jobPost')
+        console.log(clickedJobPost);
+        const clickedJobID = clickedJobPost.attributes[1].value;
+
+        app.renderDetails(clickedJobID, posts);
+      }
+
+      jobPosts.forEach(post => post.addEventListener('click', showDetails));
     };
+
+    // Print details onto page using ID reference for index selection
+    app.renderDetails = (jobID, posts) => {
+      const jobDescription = document.querySelector('.jobDescription');
+
+      jobDescription.insertAdjacentHTML('beforeend', posts[jobID].jobDetails);
+    }
     
     // Initialize the app
     app.init = () => {
-      app.getJobs();
-      app.handleJopPostClick();
+      app.getJobData();
     }
 
     
